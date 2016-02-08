e8f16030cca5cd912188366f39471d53426b4c4f
Tom MacWright <tom@macwright.org>
2016-02-08 13:58:06 -0500

    Regenerate fixtures

--- test/donuts.js
+++ test/donuts.js
@@ -3,13 +3,13 @@ var donuts = require('../'),
   glob = require('glob'),
   fs = require('fs')
 
-var REGEN = false;
+var REGEN = process.env.REGEN;
 
 test('intersect', function(t){
   glob.sync(__dirname + '/fixtures/in/*.geojson').forEach(function(input) {
       var fcs = JSON.parse(fs.readFileSync(input));
       var output = donuts(fcs);
-      if (REGEN) fs.writeFileSync(input.replace('/in/', '/out/'), JSON.stringify(output));
+      if (REGEN) fs.writeFileSync(input.replace('/in/', '/out/'), JSON.stringify(output, null, 2));
       t.deepEqual(output, JSON.parse(fs.readFileSync(input.replace('/in/', '/out/'))), input);
   });
   t.end();
